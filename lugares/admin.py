3ded229e6fe221427b21b627b2d3658a75c2e023
Axelio <diaz.axelio@gmail.com>
2014-02-04 22:01:46 -0430

    Añadiendo soporte de lugares

---
+++ lugares/admin.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+from lugares.models import *
+
+admin.site.register(Estado)
+admin.site.register(Pais)
+admin.site.register(Institucion)
+
---
+++ lugares/models.py
@@ -0,0 +1,49 @@
+# -*- coding: UTF8 -*-
+from django.db import models
+
+class Pais(models.Model):
+    '''
+    Modelo para la clase País
+    '''
+    nombre = models.CharField(max_length=50)
+    class Meta:
+    	db_table = u'país'
+    	verbose_name = u'país'
+    	verbose_name_plural = u'países'
+    def __unicode__(self):
+    	return "%s" % (self.nombre)
+
+class Estado(models.Model):
+    '''
+    Modelo para la clase Estado
+    '''
+    nombre = models.CharField(max_length=100,)
+    pais = models.ForeignKey(Pais,verbose_name=u'país')
+    class Meta:
+        db_table = u'estado'
+    def __unicode__(self):
+        return u'%s'%(self.nombre)
+
+class Institucion(models.Model):
+    '''
+    Modelo para la clase Estado
+    '''
+    nombre = models.CharField(max_length=150,)
+    estado = models.ForeignKey('Estado')
+    class Meta:
+        db_table            = u'institucion'
+        verbose_name_plural = u'instituciones'
+        verbose_name        = u'institución'
+    def __unicode__(self):
+        return self.nombre
+
+class Nacionalidad(models.Model):
+    '''
+    Modelo para la clase Nacionalidad
+    '''
+    nombre = models.CharField(max_length=150,)
+    class Meta:
+        db_table            = u'nacionalidad'
+        verbose_name_plural = u'nacionalidades'
+    def __unicode__(self):
+        return self.nombre
