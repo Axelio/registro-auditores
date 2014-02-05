42a29db5d8da51839f0ba14569a1145cc8e0fd67
Axelio <diaz.axelio@gmail.com>
2014-02-04 22:00:28 -0430

    Corrigiendo zona horaria y configuración de Suit

--- auditores_suscerte/settings.py
+++ auditores_suscerte/settings.py
@@ -71,7 +71,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'es-ve'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Caracas'
 
 USE_I18N = True
 
@@ -96,35 +96,3 @@ STATICFILES_DIRS = (
 os.path.join(os.getcwd(), 'main/static/public'), 
 )
 
-'''
-Django-Suit config
-'''
-SUIT_CONFIG = {
-    # header
-    'ADMIN_NAME': 'Axelio',
-    'HEADER_DATE_FORMAT': 'l, j. F Y',
-    'HEADER_TIME_FORMAT': 'H:i',
-
-    # forms
-    'SHOW_REQUIRED_ASTERISK': True,  # Default True
-    'CONFIRM_UNSAVED_CHANGES': True, # Default True
-
-    # menu
-    # 'SEARCH_URL': '/admin/auth/user/',
-    'MENU_ICONS': {
-       'sites': 'icon-leaf',
-       'auth': 'icon-lock',
-    },
-    # 'MENU_OPEN_FIRST_CHILD': True, # Default True
-    # 'MENU_EXCLUDE': ('auth.group',),
-    # 'MENU': (
-    #     'sites',
-    #     {'app': 'auth', 'icon':'icon-lock', 'models': ('user', 'group')},
-    #     {'label': 'Settings', 'icon':'icon-cog', 'models': ('auth.user', 'auth.group')},
-    #     {'label': 'Support', 'icon':'icon-question-sign', 'url': '/support/'},
-    # ),
-
-    # misc
-    'LIST_PER_PAGE': 15,
-    'VERSION': 0.1,
-}
