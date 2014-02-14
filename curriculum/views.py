262c08ff58dc1923ddb08f229299d14098cc9af8
Axel Díaz axelio <diaz.axelio@gmail.com>
2014-02-14 14:43:15 -0430

    Funcionamiento basico del formulario listo

--- curriculum/views.py
+++ curriculum/views.py
@@ -7,6 +7,7 @@ from curriculum.models import *
 from curriculum.forms import *
 from personas.models import *
 from personas.forms import *
+import datetime
 
 class CurriculumView(View):
     '''
@@ -31,23 +32,26 @@ class CurriculumView(View):
 
         email2_error = ''
         error_general = ''
+        error = False
 
         if not request.POST.has_key('email_2'):
             email2_error = u'Este campo es obligatorio.'
             self.persona_form.errors = True
+            error = True
         else:
             valor_email2 = request.POST['email_2']
 
         if not self.persona_form.errors:
             if not request.POST['email'] == request.POST['email_2']:
+                error = True
                 error_general = u'Ambas direcciones de correo electrónico deben coincidir. Por favor, revise y vuelva a intentarlo.'
 
         # Revisar si ya hay alguna cédula o email guardada para este usuario
-        if Persona.objects.filter(cedula=request.POST['cedula']):
-            self.persona_form.errors = True
+        if Persona.objects.filter(cedula=request.POST['cedula']).exists():
+            error = True
             error_general = u'Esta persona ya se encuentra registrada con esa cédula.'
-        if Persona.objects.filter(email=request.POST['email']):
-            self.persona_form.errors = True
+        if Persona.objects.filter(email=request.POST['email']).exists() or User.objects.filter(email=request.POST['email']).exists():
+            error = True
             error_general = u'Esta persona ya se encuentra registrada con ese email.'
 
         self.diccionario.update(csrf(request))
@@ -57,39 +61,38 @@ class CurriculumView(View):
         self.diccionario.update({'error_general':error_general})
 
         # Si hay algún error, se renderiza de nuevo la plantilla con los errores encontrados
-        if self.persona_form.errors:
+        if error:
             return render(request, 
                            template_name=self.template,
                            dictionary=self.diccionario,
                          )
         else:
-             # Si no hay ningún error entonces se crea la persona:
-             persona = Persona.objects.create(cedula=request.POST['cedula'],
-                                              primer_nombre = request.POST['primer_nombre'],
-                                              segundo_nombre = request.POST['segundo_nombre'],
-                                              primer_apellido = request.POST['primer_apellido'],
-                                              segundo_apellido = request.POST['segundo_apellido'],
-                                              genero = request.POST['genero'],
-                                              reside = request.POST['reside'],
-                                              direccion = request.POST['direccion'],
-                                              fecha_nacimiento = request.POST['fecha_nacimiento'],
-                                              tlf_reside = request.POST['tlf_reside'],
-                                              tlf_movil = request.POST['tlf_movil'],
-                                              tlf_oficina = request.POST['tlf_oficina'],
-                                              tlf_contacto = request.POST['tlf_contacto'],
-                                              estado_civil = request.POST['estado_civil'],
-                                              email = request.POST['email'],
-                                              )
-
-             # Se crea el usuario con el correo electrónico por defecto y se crea una contraseña aleatoria para el usuario
-             clave = User.objects.make_random_password()
-             print clave
-             usuario = User.objects.create_user(username = request.POST['email'],
-                                               email = request.POST['email'], 
-                                               first_name = request.POST['primer_nombre'],
-                                               last_name = request.POST['segundo_nombre'],
-                                               password = clave, 
-                                              )
+            estado = Estado.objects.get(id=request.POST['reside'])
+            fecha_nacimiento = datetime.datetime.strptime(request.POST['fecha_nacimiento'], "%d/%m/%Y").strftime("%Y-%m-%d") 
+            persona = Persona.objects.create(cedula=request.POST['cedula'],
+                                             primer_nombre = request.POST['primer_nombre'],
+                                             segundo_nombre = request.POST['segundo_nombre'],
+                                             primer_apellido = request.POST['primer_apellido'],
+                                             segundo_apellido = request.POST['segundo_apellido'],
+                                             genero = request.POST['genero'],
+                                             reside = estado,
+                                             direccion = request.POST['direccion'],
+                                             fecha_nacimiento = fecha_nacimiento,
+                                             tlf_reside = request.POST['tlf_reside'],
+                                             tlf_movil = request.POST['tlf_movil'],
+                                             tlf_oficina = request.POST['tlf_oficina'],
+                                             tlf_contacto = request.POST['tlf_contacto'],
+                                             estado_civil = request.POST['estado_civil'],
+                                             email = request.POST['email'],
+                                             )
+            # Se crea el usuario con el correo electrónico por defecto y se crea una contraseña aleatoria para el usuario
+            clave = User.objects.make_random_password()
+            usuario = User.objects.create_user(username = request.POST['email'],
+                                              email = request.POST['email'], 
+                                              first_name = request.POST['primer_nombre'],
+                                              last_name = request.POST['segundo_nombre'],
+                                              password = clave, 
+                                             )
 
         return render(request, 
                        template_name=self.template,
