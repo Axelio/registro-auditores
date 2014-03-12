196f477a72a1be0b71bd56e6ae8bd45f7f9aa21d
Axel Díaz axelio <diaz.axelio@gmail.com>
2014-03-12 10:18:12 -0430

    Eliminando funcion innecesaria

--- auth/views.py
+++ auth/views.py
@@ -62,57 +62,6 @@ class Auth2(View):
                            dictionary=self.diccionario,
                          )
 
-# Create your views here.
-class CambiarClave(View):
-    tipo_mensaje = ''
-    mensaje = ''
-    #form = PasswordChangeForm
-    form = CambiarClaveForm
-    template = 'perfil/editar_formulario.html'
-    titulo = 'cambiar contraseña'
-    diccionario = {}
-
-    diccionario.update({'titulo':titulo})
-    
-    def get(self, request, *args, **kwargs):
-        '''
-        Si el usuario está autenticado pasamos formulario CambiarClaveLoged
-        '''
-        import pdb
-        #pdb.set_trace()
-        if request.user.is_authenticated():
-            self.diccionario.update({'user':request.user})
-            self.diccionario.update({'tipo_mensaje':self.tipo_mensaje})
-            self.diccionario.update({'mensaje':self.mensaje})
-            self.diccionario.update({'formulario':self.form})
-            return render(request, 
-                           template_name=self.template,
-                           dictionary=self.diccionario,
-                         )
-
-    def post(self, request, *args, **kwargs):
-        form = self.form(request.POST)
-        self.diccionario.update({'user':request.user})
-        self.diccionario.update({'tipo_mensaje':self.tipo_mensaje})
-        self.diccionario.update({'mensaje':self.mensaje})
-        self.diccionario.update({'formulario':form})
-        if form.is_valid():
-            usuario = request.user
-            login(request, form.get_user())
-            if request.GET.has_key('next'):
-                return HttpResponseRedirect(request.GET['next'])
-        else:
-            error = ''
-            error = form.errors[form.errors.keys()[0]]
-
-            self.diccionario.update({'tipo_mensaje':'error'})
-            self.diccionario.update({'error':error[0]})
-
-            return render(request, 
-                           template_name=self.template,
-                           dictionary=self.diccionario,
-                         )
-
 def cambiar_clave(request):
     diccionario = {}
     titulo = 'cambiar contraseña'
