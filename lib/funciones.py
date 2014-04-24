03190c307960379e07c6c1c0b433ae6b6c2c8220
Axel Díaz axelio <diaz.axelio@gmail.com>
2014-04-24 09:46:03 -0430

    Añadiendo función para determinar fechas pasadas

--- lib/funciones.py
+++ lib/funciones.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import datetime
 
+
 def fecha_futura(fecha):
     '''
     Funcion para determinar si la fecha es o no futura.
@@ -8,9 +9,26 @@ def fecha_futura(fecha):
     '''
     # Fecha actual
     fecha_actual = datetime.date.today()
-    # Se revisa si la fecha del formulario es futura (fecha de formulario mayor a la actual)
+    # Se revisa si la fecha del formulario es futura
+    # (fecha de formulario mayor a la actual)
     if fecha >= fecha_actual:
         # Se retorna True dado que la fecha facilitada es futura
+        return True
+    else:
+        return False
+
+
+def fecha_pasada(fecha):
+    '''
+    Funcion para determinar si la fecha es o no pasada.
+    La función debe retornar True si la fecha es pasada.
+    '''
+    # Fecha actual
+    fecha_actual = datetime.date.today()
+    # Se revisa si la fecha del formulario es pasada
+    # (fecha de formulario menor a la actual)
+    if fecha <= fecha_actual:
+        # Se retorna True dado que la fecha facilitada es pasada
         return True
     else:
         return False
