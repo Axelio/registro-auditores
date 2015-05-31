a31831436657cbbd9e7c4d0a294aa2a869a039a2
Axel Díaz <diaz.axelio@gmail.com>
2015-05-31 09:30:21 -0430

    Corrigiendo enlaces de perfil

--- perfil/urls.py
+++ perfil/urls.py
@@ -11,43 +11,43 @@ from .views import PerfilView, CrearPerfilView, EditarPerfilView
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^perfil/citas/$',
+    url(r'^citas/$',
         login_required(CitasView.as_view()),
         name='citas'),
 
-    url(r'^perfil/educacion/(?P<palabra>\w+)/(?P<educacion_id>[\d]+)*$',
+    url(r'^educacion/(?P<palabra>\w+)/(?P<educacion_id>[\d]+)*$',
         login_required(EducacionView.as_view()),
         name='educacion'),
 
-    url(r'^perfil/laboral/(?P<palabra>\w+)/(?P<laboral_id>[\d]+)*$',
+    url(r'^laboral/(?P<palabra>\w+)/(?P<laboral_id>[\d]+)*$',
         login_required(LaboralView.as_view()),
         name='laboral'),
 
-    url(r'^perfil/conocimiento/(?P<palabra>\w+)/(?P<conocimiento_id>[\d]+)*$',
+    url(r'^conocimiento/(?P<palabra>\w+)/(?P<conocimiento_id>[\d]+)*$',
         login_required(ConocimientoView.as_view()),
         name='conocimiento'),
 
-    url(r'^perfil/evaluacion/(?P<evaluacion_id>[\d]+)/(?P<aspirante_id>[\d]+)/',
+    url(r'^evaluacion/(?P<evaluacion_id>[\d]+)/(?P<aspirante_id>[\d]+)/',
         login_required(EvaluacionView.as_view()),
         name='evaluacion'),
 
-    url(r'^perfil/competencia/(?P<aspirante_id>[\d]+)/',
+    url(r'^competencia/(?P<aspirante_id>[\d]+)/',
         login_required(CompetenciaView.as_view()),
         name='competencia'),
 
-    url(r'^perfil/habilidad/(?P<palabra>\w+)/(?P<habilidad_id>[\d]+)*$',
+    url(r'^habilidad/(?P<palabra>\w+)/(?P<habilidad_id>[\d]+)*$',
         login_required(HabilidadView.as_view()),
         name='habilidad'),
 
-    url(r'^perfil/idioma/(?P<palabra>\w+)/(?P<idioma_id>[\d]+)*$',
+    url(r'^idioma/(?P<palabra>\w+)/(?P<idioma_id>[\d]+)*$',
         login_required(IdiomaView.as_view()),
         name='idioma'),
 
-    url(r'^perfil/curso/(?P<palabra>\w+)/(?P<curso_id>[\d]+)*$',
+    url(r'^curso/(?P<palabra>\w+)/(?P<curso_id>[\d]+)*$',
         login_required(CursoView.as_view()),
         name='curso'),
 
-    url(r'^perfil/certificacion/(?P<palabra>\w+)/(?P<certificacion_id>[\d]+)*$',
+    url(r'^certificacion/(?P<palabra>\w+)/(?P<certificacion_id>[\d]+)*$',
         login_required(CertificacionView.as_view()),
         name='certificacion'),
 
@@ -55,7 +55,7 @@ urlpatterns = patterns('',
         login_required(CrearPerfilView.as_view()),
         name='crear_perfil'),
 
-    url(r'^editar/*$',
+    url(r'^editar/',
         login_required(EditarPerfilView.as_view()),
         name='editar_perfil'),
 
