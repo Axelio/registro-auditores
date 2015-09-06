b8732684f608a0bbf77d44d178821cee323e4cef
Axel Díaz <diaz.axelio@gmail.com>
2015-09-06 17:24:53 -0430

    Completando los tests de Laboral

--- perfil/tests/test_urls.py
+++ perfil/tests/test_urls.py
@@ -4,11 +4,10 @@ from django.core.urlresolvers import resolve
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
-from authentication.tests import LoginTest
 from authentication.models import UserProfile
 from lugares.models import Estado, Pais, Institucion
 from personas.models import Persona
-from curriculum.models import Educacion, TipoEducacion
+from curriculum.models import Educacion, TipoEducacion, Laboral
 
 import datetime
 
@@ -53,20 +52,179 @@ class PerfilTest(TestCase):
         resolver = self.client.get('/perfil/{0}/'.format(self.persona.id))
         self.assertEqual(resolver.status_code, 200)
 
-    def test_citas(self):
+
+class EducacionTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+        self.user = User.objects.create_user('batman',
+                                             'batman@ligadelajusticia.com',
+                                             'robin')
+
+        self.pais = Pais.objects.create(nombre='Venezuela')
+
+        self.estado = Estado.objects.create(nombre='Aragua', pais=self.pais)
+
+        self.nacimiento = datetime.datetime(1990, 8, 20)
+
+        self.persona = Persona.objects.create(cedula='1234567',
+                                              primer_nombre='Nombre1',
+                                              segundo_nombre='Nombre2',
+                                              primer_apellido='Apellido1',
+                                              segundo_apellido='Apellido2',
+                                              genero='0',
+                                              reside=self.estado,
+                                              fecha_nacimiento=self.nacimiento,
+                                              tlf_reside='01231234567',
+                                              tlf_movil='01231234567',
+                                              tlf_oficina='01231234567',
+                                              tlf_contacto='tlf_reside',
+                                              estado_civil='s',
+                                              email='correo@example.com')
+
+        self.userprofile = UserProfile.objects.create(user=self.user,
+                                                      persona=self.persona)
+
+    def test_add(self):
+        '''
+        Se prueba el enlace para agregar nueva educacion
+        '''
+        # Se deslogea todo usuario
         self.client.logout()
-        resolver = self.client.get(reverse('citas'))
+
+        # Se almacenan los datos de la URL de Educacion
+        resolver = self.client.get(
+                reverse('educacion', kwargs={'palabra': 'nueva'}))
+
+        # Se comprueban los codigos de estado del intento de ingreso
         self.assertEqual(resolver.status_code, 302)
 
+        # Batman se logea
         self.client.login(username='batman', password='robin')
-        resolver = self.client.get(reverse('citas'))
-        # self.assertEqual(resolver.status_code, 200)
+
+        # Se almacenan los datos de la URL de Educacion
+        resolver = self.client.get(
+                reverse('educacion', kwargs={'palabra': 'nueva'}))
+
+        # Se comprueban los codigos de estado del intento de ingreso
+        self.assertEqual(resolver.status_code, 200)
+
+    def test_edit(self):
+        '''
+        Se prueba el enlace para agregar nueva educacion
+        '''
+        # Se deslogea todo usuario
+        self.client.logout()
+
+        # Se almacenan los datos de la URL de Educacion
+        resolver = self.client.get(
+                reverse('educacion', kwargs={'palabra': 'editar'}))
+
+        # Se comprueban los codigos de estado del intento de ingreso
+        self.assertEqual(resolver.status_code, 302)
+
+        # Batman se logea
+        self.client.login(username='batman', password='robin')
+
+        # Se almacenan los datos de la URL de Educacion
+        resolver = self.client.get(
+                reverse('educacion', kwargs={'palabra': 'editar'}))
+
+        # Se comprueban los codigos de estado del intento de ingreso
+        self.assertEqual(resolver.status_code, 200)
+
+
+class LaboralTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+        self.user = User.objects.create_user('batman',
+                                             'batman@ligadelajusticia.com',
+                                             'robin')
+
+        self.pais = Pais.objects.create(nombre='Venezuela')
+
+        self.estado = Estado.objects.create(nombre='Aragua', pais=self.pais)
+
+        self.nacimiento = datetime.datetime(1990, 8, 20)
+
+        self.persona = Persona.objects.create(
+                cedula='1234567', primer_nombre='Nombre1',
+                segundo_nombre='Nombre2', primer_apellido='Apellido1',
+                segundo_apellido='Apellido2', genero='0', reside=self.estado,
+                fecha_nacimiento=self.nacimiento, tlf_reside='01231234567',
+                tlf_movil='01231234567', tlf_oficina='01231234567',
+                tlf_contacto='tlf_reside', estado_civil='s',
+                email='correo@example.com')
+
+        self.userprofile = UserProfile.objects.create(user=self.user,
+                                                      persona=self.persona)
+        self.f_inicio = datetime.datetime(1994, 8, 20)
+
+        self.f_fin = datetime.datetime(2000, 8, 20)
+
+        self.laboral = Laboral.objects.create(
+                usuario=self.userprofile, empresa='Liga de la Justicia',
+                sector='Social', estado=self.estado, telefono='5555555',
+                cargo='Superheroe', funcion='Salvar al mundo',
+                fecha_inicio=self.f_inicio, fecha_fin=self.f_fin,
+                trabajo_actual=True, retiro='',
+                direccion_empresa='Ciudad Gotica')
+
+    def test_add(self):
+        '''
+        Se prueba el enlace para agregar nueva informacion laboral
+        '''
+        # Se deslogea todo usuario
+        self.client.logout()
+
+        # Se almacenan los datos de la URL de Educacion
+        resolver = self.client.get(
+                reverse('laboral', kwargs={'palabra': 'nueva'}))
+
+        # Se comprueban los codigos de estado del intento de ingreso
+        self.assertEqual(resolver.status_code, 302)
+
+        # Batman se logea
+        self.client.login(username='batman', password='robin')
+
+        # Se almacenan los datos de la URL de Educacion
+        resolver = self.client.get(
+                reverse('educacion', kwargs={'palabra': 'nueva'}))
+
+        # Se comprueban los codigos de estado del intento de ingreso
+        self.assertEqual(resolver.status_code, 200)
+
+    def test_edit(self):
+        '''
+        Se prueba el enlace para agregar nueva educacion
+        '''
+        # Se deslogea todo usuario
+        self.client.logout()
+
+        # Se almacenan los datos de la URL de Educacion
+        resolver = self.client.get(
+                reverse('laboral', kwargs={'palabra': 'editar'}))
+
+        # Se comprueban los codigos de estado del intento de ingreso
+        self.assertEqual(resolver.status_code, 302)
+
+        # Batman se logea
+        self.client.login(username='batman', password='robin')
+
+        # Se almacenan los datos de la URL de Educacion
+        resolver = self.client.get(
+                reverse('laboral', kwargs={'palabra': 'editar'}))
+
+        # Se comprueban los codigos de estado del intento de ingreso
+        self.assertEqual(resolver.status_code, 200)
 
 '''
-
-    def test_laboral(self):
         resolver = self.client.get(reverse('laboral'))
         # self.assertEqual(resolver.status_code, 302)
+
 
     def test_conocimiento(self):
         resolver = self.client.get(reverse('conocimiento'))
--- perfil/tests/test_views.py
+++ perfil/tests/test_views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from authentication.models import UserProfile
 from personas.models import Persona, Estado
 from lugares.models import Estado, Pais, Institucion
-from curriculum.models import Educacion, TipoEducacion
+from curriculum.models import Educacion, TipoEducacion, Laboral
 
 import datetime
 
@@ -83,16 +83,16 @@ class EducacionTest(TestCase):
                                                   fecha_inicio=self.f_inicio,
                                                   fecha_fin=self.f_fin)
 
+        # Se preparan los datos para enviarlos a la vista
+        self.f_inicio = self.f_inicio.strftime('%d/%m/%Y')
+        self.f_fin = self.f_fin.strftime('%d/%m/%Y')
+
     def test_create(self):
         # Se logea al usuario
         self.client.login(username='batman', password='robin')
 
         # Se imprimen las educaciones antes del test
         self.assertEquals(Educacion.objects.count(), 1)
-
-        # Se preparan los datos para enviarlos a la vista
-        self.f_inicio = self.f_inicio.strftime('%d/%m/%Y')
-        self.f_fin = self.f_fin.strftime('%d/%m/%Y')
 
         # Se almacenan los datos necesarios en el diccionario "post"
         post = {'persona': self.persona, 'titulo': 'Superheroe',
@@ -105,6 +105,29 @@ class EducacionTest(TestCase):
 
         # Se comprueba que haya una nueva Educacion
         self.assertEquals(Educacion.objects.count(), 2)
+
+    def test_edit(self):
+        # Se logea al usuario
+        self.client.login(username='batman', password='robin')
+
+        # Se imprimen las educaciones antes del test
+        self.assertEquals(self.educacion.titulo, 'Superheroe')
+
+        # Se almacenan los datos necesarios en el diccionario "post"
+        post = {'persona': self.persona, 'titulo': 'Guardian de Ciudad Gotica',
+                'institucion': self.institucion, 'tipo': self.tipo_educacion,
+                'fecha_inicio': self.f_inicio, 'fecha_fin': self.f_fin}
+
+        # Se envian los datos a la vista para crear una nueva Educacion
+        resolve = self.client.post(
+                reverse('educacion', kwargs={
+                    'palabra': 'editar', 'educacion_id': self.educacion.id}),
+                post)
+
+        self.educacion = Educacion.objects.first()
+
+        # Se comprueba que haya una nueva Educacion
+        self.assertEquals(self.educacion.titulo, 'Guardian de Ciudad Gotica')
 
     def test_delete(self):
         # Se logea al usuario
@@ -125,3 +148,117 @@ class EducacionTest(TestCase):
 
         # Se comprueba que no haya ninguna  Educacion
         self.assertEquals(Educacion.objects.count(), 0)
+
+
+class LaboralTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+        self.user = User.objects.create_user('batman',
+                                             'batman@ligadelajusticia.com',
+                                             'robin')
+
+        self.pais = Pais.objects.create(nombre='Venezuela')
+
+        self.estado = Estado.objects.create(nombre='Aragua', pais=self.pais)
+
+        self.nacimiento = datetime.datetime(1990, 8, 20)
+
+        self.persona = Persona.objects.create(
+                cedula='1234567', primer_nombre='Nombre1',
+                segundo_nombre='Nombre2', primer_apellido='Apellido1',
+                segundo_apellido='Apellido2', genero='0', reside=self.estado,
+                fecha_nacimiento=self.nacimiento, tlf_reside='01231234567',
+                tlf_movil='01231234567', tlf_oficina='01231234567',
+                tlf_contacto='tlf_reside', estado_civil='s',
+                email='correo@example.com')
+
+        self.userprofile = UserProfile.objects.create(user=self.user,
+                                                      persona=self.persona)
+
+        self.f_inicio = datetime.datetime(1994, 8, 20)
+
+        self.f_fin = datetime.datetime(2000, 8, 20)
+
+        self.laboral = Laboral.objects.create(
+                usuario=self.userprofile, empresa='Liga de la Justicia',
+                sector='Social', estado=self.estado, telefono='5555555',
+                cargo='Superheroe', funcion='Salvar al mundo',
+                fecha_inicio=self.f_inicio, fecha_fin=self.f_fin,
+                trabajo_actual=True, retiro='Ninguna',
+                direccion_empresa='Ciudad Gotica')
+
+        # Se preparan los datos para enviarlos a la vista
+        self.f_inicio = self.f_inicio.strftime('%d/%m/%Y')
+        self.f_fin = self.f_fin.strftime('%d/%m/%Y')
+
+    def test_create(self):
+        # Se logea al usuario
+        self.client.login(username='batman', password='robin')
+
+        # Se imprimen las informaciones laborales antes del test
+        self.assertEquals(Laboral.objects.count(), 1)
+
+        # Se almacenan los datos necesarios en el diccionario "post"
+        post = {'usuario': self.userprofile, 'empresa': 'Liga de la Justicia',
+                'sector': 'Social', 'estado': self.estado.id,
+                'telefono': '5555555', 'cargo': 'Superheroe',
+                'funcion': 'Salvar al mundo', 'fecha_inicio': self.f_inicio,
+                'fecha_fin': self.f_fin, 'trabajo_actual': True, 'retiro': 'Ninguna',
+                'direccion_empresa': 'Ciudad Gotica'}
+
+        # Se envian los datos a la vista para crear una nueva
+        # informacion Laboral
+        resolve = self.client.post(
+                reverse('laboral', kwargs={'palabra': 'nueva'}), post)
+
+        # Se comprueba que haya una nueva informacion Laboral
+        self.assertEquals(Laboral.objects.count(), 2)
+
+    def test_edit(self):
+        # Se logea al usuario
+        self.client.login(username='batman', password='robin')
+
+        # Se imprimen las informaciones laborales antes del test
+        self.assertEquals(self.laboral.cargo, 'Superheroe')
+
+        # Se almacenan los datos necesarios en el diccionario "post"
+        post = {'usuario': self.userprofile, 'empresa': 'Liga de la Justicia',
+                'sector': 'Social', 'estado': self.estado.id,
+                'telefono': '5555555', 'cargo': 'Guardian de Ciudad Gotica',
+                'funcion': 'Salvar al mundo', 'fecha_inicio': self.f_inicio,
+                'fecha_fin': self.f_fin, 'trabajo_actual': True, 'retiro': 'Ninguna',
+                'direccion_empresa': 'Ciudad Gotica'}
+
+        # Se envian los datos a la vista para crear una nueva
+        # informacion Laboral
+        resolve = self.client.post(
+                reverse('laboral', kwargs={
+                    'palabra': 'editar', 'laboral_id': self.laboral.id}),
+                post)
+
+        self.laboral= Laboral.objects.first()
+
+        # Se comprueba que se haya editado el cargo laboral
+        self.assertEquals(self.laboral.cargo, 'Guardian de Ciudad Gotica')
+
+    def test_delete(self):
+        # Se logea al usuario
+        self.client.login(username='batman', password='robin')
+
+        # Se comparan los Laborales antes del test
+        self.assertEquals(Laboral.objects.count(), 1)
+
+        # Se busca el primer object Educacion
+        self.laboral = laboral.objects.first()
+
+        # Se envian los datos a la vista para eliminar la info laboral
+        resolve = self.client.get(
+                reverse('laboral',
+                        kwargs={
+                            'palabra': 'eliminar',
+                            'laboral_id': self.laboral.id}))
+
+        # Se comprueba que no haya ninguna info laboral
+        self.assertEquals(Laboral.objects.count(), 0)
