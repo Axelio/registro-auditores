5bc0f69c02bf655f8a6efd5d14df82f6e7ae68ae
Axel Díaz <diaz.axelio@gmail.com>
2015-09-08 10:58:15 -0430

    Añadiendo puntualización de modelo

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF8 -*-
 from django.contrib import admin
-from models import UserProfile, Mensaje
+from .models import UserProfile, Mensaje
 from django.contrib.auth.admin import UserAdmin, User
 from django.utils.translation import ugettext_lazy as _
 from django.utils.decorators import method_decorator
@@ -8,14 +8,14 @@ from django.utils.decorators import method_decorator
 
 class UserProfileInline(admin.StackedInline):
     model=UserProfile
-    can_delete=False 
+    can_delete=False
     extra=1
     max_num = 1
 
 
 class UserProfileAdmin(UserAdmin):
     search_fields=['userprofile__persona__num_identificacion','username','email',]
-    inlines=[UserProfileInline, ]    
+    inlines=[UserProfileInline, ]
     staff_fieldsets = (
         (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
@@ -48,14 +48,14 @@ class UserProfileAdmin(UserAdmin):
         ''' Si se esta editando el mismo usuario  y no es superusuario se muestran los grupos a los que pertenece el usuario, si no es el mismo usuario solo muestra estudiantes '''
         '''
         if not request.user.is_superuser:
-            from django.db.models import Q 
+            from django.db.models import Q
             if obj==request.user:
                 form.base_fields['groups'].queryset=(grupos|request.user.groups.all()).distinct()
             else:
                 form.base_fields['groups'].queryset=grupos
         '''
         return form
-    
+
     ''' Evita escalabilidad de privilegios cuando  un usuario staff NO superUsuario intente cambiar la clave de un SuperUsuario. '''
     from django.views.decorators.debug import sensitive_post_parameters
     @method_decorator(sensitive_post_parameters())
@@ -66,8 +66,8 @@ class UserProfileAdmin(UserAdmin):
         else:
             return super(UserProfileAdmin,self).user_change_password(request,id,form_url)
 
-    
-    ''' 
+
+    '''
     No mostrar la opcion de SuperUsuario si no se es un superusuario. Evitar bug de escalabilida de permisos
     '''
     def change_view(self, request, *args, **kwargs):
@@ -75,7 +75,7 @@ class UserProfileAdmin(UserAdmin):
         if not request.user.is_superuser:
             try:
                 #Si el usuario editado es un SuperUsuario y el editor no lo es,o Si el usuario editado es staff  NO se muestra el enlace de cambiar clave. Evitando escalabilidad de privilegios
-                
+
                 if self.queryset(request).get(pk=args[0]).is_superuser or self.queryset(request).get(pk=args[0]).is_staff:
                     self.fieldsets=self.staff_fieldsets_sinpass
                 else:
