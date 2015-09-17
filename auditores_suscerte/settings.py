137420cc213886dc13d1ae3d0c3b064f03230d32
Axel Díaz <diaz.axelio@gmail.com>
2015-09-17 02:40:35 -0430

    Corrigiendo base de datos

--- auditores_suscerte/settings.py
+++ auditores_suscerte/settings.py
@@ -191,3 +191,22 @@ ALLOWED_HOSTS = ['*']
 
 # Activacion de notificaciones por correo
 NOTIFY = False
+
+import dj_database_url
+try:
+    DATABASES['default'] =  dj_database_url.config()
+except:
+    # Database
+    # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            # 'NAME': 'auditores',
+            # 'USER': 'auditores',
+            # 'PASSWORD': 'auditores',
+            # 'HOST': '127.0.0.1',
+            # 'PORT': '5432',
+        }
+    }
