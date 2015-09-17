0948e8428b422080b5f40927727ad4cf3712debb
Axel Díaz <diaz.axelio@gmail.com>
2015-09-17 03:47:30 -0430

    Mejorando detalles de aparicion de epicentro y lugares cercanos

--- geomap/views.py
+++ geomap/views.py
@@ -28,21 +28,24 @@ GEOCODE_BASE_URL = 'https://maps.googleapis.com/maps/api/place/radarsearch/json'
 def MapaJson(address, **geo_args):
 
     geometry = []
+    epicentro = {}
     for ubicacion in Ubicacion.objects.all():
-        diccionario = {
+        geo_args.update({
             'location': '{0},{1}'.format(ubicacion.lat, ubicacion.lng),
             'radius': 500,
             'types': 'restaurant',#|cafe|food',
             'key': 'AIzaSyDoklHqzjuOTLDx14BM4mBq1bRf1vFd0Ts'
-                }
-        geo_args.update(diccionario)
+                })
         #https://maps.googleapis.com/maps/api/place/radarsearch/json?location=51.503186,-0.126446&radius=5000&types=museum&key=AIzaSyDoklHqzjuOTLDx14BM4mBq1bRf1vFd0Ts
         url = GEOCODE_BASE_URL + '?' + urllib.urlencode(geo_args)
         url = urllib.url2pathname(url)
         result = simplejson.load(urllib.urlopen(url))
+        epicentro['epi_lat'] = float(ubicacion.lat)
+        epicentro['epi_lng'] = float(ubicacion.lng)
+        for resultado in result['results']:
+            resultado.update(epicentro)
 
         geometry.append([s for s in result['results']])
-
     consulta = simplejson.dumps(geometry, indent=2)
     return HttpResponse(consulta, content_type="application/json")
 
